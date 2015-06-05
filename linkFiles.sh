a9a256004592ebf9c6c947d6762a3d7c0628654d
Rickard Gimerstedt <rickard@gimerstedt.se>
2015-06-05 17:20:07 +0200

    renaming for reasons

--- linkFiles.sh
+++ linkFiles.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 HERE=$(pwd)
-SCRIPTS=(.bashrc .dircolors .inputrc .tmux.conf .vimrc .zshrc)
+SCRIPTS=(bashrc config dircolors inputrc tmux.conf vimrc xinitrc Xresources zshrc)
 mkdir -p backup
 
 for index in ${!SCRIPTS[*]}
 do
 	S=${SCRIPTS[$index]}
 	printf "backing up $HOME/$S to $HERE/backup/$S\n"
-	mv $HOME/$S $HERE/backup
-	printf "linking $S to $HOME/$S\n"
-	ln -s $HERE/$S $HOME/$S
+	mv $HOME/.$S $HERE/backup
+	printf "linking $S to $HOME/.$S\n"
+	ln -s $HERE/$S $HOME/.$S
 done
 
 #mkdir -p backup && mv ~/.bashrc backup
