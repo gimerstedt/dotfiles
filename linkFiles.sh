b80a46fc7b8fc774a7b61572971f2702388c7b5e
ake1 <rickard@gimerstedt.se>
2015-06-05 09:44:45 +0200

    link it.

---
+++ linkFiles.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+HERE=$(pwd)
+SCRIPTS=(.bashrc .dircolors .inputrc .tmux.conf .vimrc .zshrc)
+mkdir -p backup
+
+for index in ${!SCRIPTS[*]}
+do
+	S=${SCRIPTS[$index]}
+	printf "backing up $HOME/$S to $HERE/backup/$S\n"
+	mv $HOME/$S $HERE/backup
+	printf "linking $S to $HOME/$S\n"
+	ln -s $HERE/$S $HOME/$S
+done
+
+#mkdir -p backup && mv ~/.bashrc backup
+#ln -s $HERE/.bashrc ~/.bashrc
