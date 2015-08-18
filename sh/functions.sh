e9e35ed95d8c0ca2043bceec8f7705f2b4fd1d8c
Rickard Gimerstedt <rickard@gimerstedt.se>
2015-08-18 07:07:38 +0200

    docker functions

--- sh/functions.sh
+++ sh/functions.sh
@@ -1,1 +1,4 @@
-function dremoveexited { docker rm -v $(docker ps -a -q -f status=exited); }
+function drmexited { docker rm $(docker ps -aqf status=exited); }
+function drmstopped { docker rm $(docker ps -aq); }
+function drmiuntagged { docker rmi $(docker images -q --filter "dangling=true"); }
+#function drmiuntagged { docker rmi $(docker images | grep "^<none>" | awk '{print $3}'); }
