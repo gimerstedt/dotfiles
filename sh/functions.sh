9731afde58de42d50e1afab0571ed48867561ddc
Rickard Gimerstedt <rickard@gimerstedt.se>
2015-09-10 19:52:49 +0200

    some ip aliases

--- sh/functions.sh
+++ sh/functions.sh
@@ -1,4 +1,3 @@
 function drmexited { docker rm $(docker ps -aqf status=exited); }
 function drmstopped { docker rm $(docker ps -aq); }
 function drmiuntagged { docker rmi $(docker images -q --filter "dangling=true"); }
-#function drmiuntagged { docker rmi $(docker images | grep "^<none>" | awk '{print $3}'); }
