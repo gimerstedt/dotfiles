65887d285d30117759dd840d5112d78dd619b697
Rickard Gimerstedt <rickard@gimerstedt.se>
2016-04-01 14:59:52 +0000

    bash profile for sourcing bashrc, removed tmux mouse stuff

--- install.sh
+++ install.sh
@@ -6,9 +6,8 @@ cp $cwd/Xresources ~/.Xresources
 cp $cwd/xinitrc ~/.xinitrc
 cp $cwd/bash/bashrc.template ~/.bashrc
 
-ln -s $cwd/emacs/spacemacs ~/.spacemacs
 ln -s $cwd/tmux/tmux.conf ~/.tmux.conf
 ln -s $cwd/vim/vimrc ~/.vimrc
 ln -s $cwd/vim/vimrc ~/.nvimrc
 ln -s $cwd/zsh/zshrc ~/.zshrc
-ln -s $cwd/bash/inputrc ~/.inputrc
+ln -s $cwd/bash/bash_profile ~/.bash_profile
