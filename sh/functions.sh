b8ed952eb4c475af9283db48c9feca039638ff0b
Rickard Gimerstedt <rickard@gimerstedt.se>
2016-04-07 17:23:41 +0200

    tmux color and this function

--- sh/functions.sh
+++ sh/functions.sh
@@ -1,3 +1,9 @@
 function drmexited { docker rm $(docker ps -aqf status=exited); }
 function drmstopped { docker rm $(docker ps -aq); }
 function drmiuntagged { docker rmi $(docker images -q --filter "dangling=true"); }
+function gcam()
+{
+	git add .
+	git commit -m "$@"
+	git pull --rebase
+}
