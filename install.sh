40095c1fc0f584f17182cccceeab4d57488b6358
Rickard Gimerstedt <rickard@gimerstedt.se>
2016-04-10 11:47:42 +0200

    removed unused stuff

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 cwd=$(pwd)
 
 cp $cwd/git/gitconfig.template ~/.gitconfig
-cp $cwd/Xresources ~/.Xresources
-cp $cwd/xinitrc ~/.xinitrc
 cp $cwd/bash/bashrc.template ~/.bashrc
 
 ln -s $cwd/tmux/tmux.conf ~/.tmux.conf
-ln -s $cwd/vim/vimrc ~/.vimrc
-ln -s $cwd/vim/vimrc ~/.nvimrc
-ln -s $cwd/zsh/zshrc ~/.zshrc
 ln -s $cwd/bash/bash_profile ~/.bash_profile
+
+mkdir -p ~/.vim/backup
+ln -s $cwd/vim/vimrc ~/.vimrc
+
+mkdir -p ~/.ssh
 ln -s $cwd/ssh/config ~/.ssh/config
