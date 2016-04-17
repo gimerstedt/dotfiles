4ee3cb648db0cb19b1192c0e3b77f828d53d1c1e
Rickard Gimerstedt <rickard@gimerstedt.se>
2016-04-17 23:12:23 +0200

    paste enabled in vim, orange prompt

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -1,6 +1,6 @@
 RED="\[$(tput setaf 1)\]"
-MAGENTA="\[$(tput setaf 5)\]"
+ORANGE="\[$(tput setaf 208)\]"
 BOLD="\[$(tput bold)\]"
 RESET="\[$(tput sgr0)\]"
 
-export PS1="\u@\h:\w\n$MAGENTA\$$RESET "
+export PS1="$ORANGE\u@\h:\w\n\$$RESET "
