cc0d096f5d97af47badcbf1ca3b60722adfe1030
Rickard Gimerstedt <rickard@gimerstedt.se>
2016-05-10 08:54:11 +0200

    killjava on win

--- sh/functions.sh
+++ sh/functions.sh
@@ -1,9 +1,14 @@
 function drmexited { docker rm $(docker ps -aqf status=exited); }
 function drmstopped { docker rm $(docker ps -aq); }
 function drmiuntagged { docker rmi $(docker images -q --filter "dangling=true"); }
+
 function gcam()
 {
 	git add .
 	git commit -m "$@"
 	git pull --rebase
+}
+
+killjava(){
+	taskkill  /PID `ps -W | grep 'java.exe' | awk '{print $1}'` /F
 }
