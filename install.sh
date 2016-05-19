813a9a09fc3db104bb1062ba19ea3edae8cd1ed8
Rickard Gimerstedt <rickard@gimerstedt.se>
2016-05-19 21:15:01 +0200

    force symlink, why not

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ cwd=$(pwd)
 cp $cwd/git/gitconfig.template ~/.gitconfig
 cp $cwd/bash/bashrc.template ~/.bashrc
 
-ln -s $cwd/tmux/tmux.conf ~/.tmux.conf
-ln -s $cwd/bash/bash_profile ~/.bash_profile
+ln -sf $cwd/tmux/tmux.conf ~/.tmux.conf
+ln -sf $cwd/bash/bash_profile ~/.bash_profile
 
 mkdir -p ~/.vim/backup
-ln -s $cwd/vim/vimrc ~/.vimrc
+ln -sf $cwd/vim/vimrc ~/.vimrc
 
 mkdir -p ~/.ssh
-ln -s $cwd/ssh/config ~/.ssh/config
+ln -sf $cwd/ssh/config ~/.ssh/config
 
-ln -s $cwd/sh/profile ~/.profile
+ln -sf $cwd/sh/profile ~/.profile
