2c9eca4e525673997c77958e2915cb91f6070a55
Rickard Gimerstedt <rickard@gimerstedt.se>
2016-05-20 14:27:19 +0200

    moved files, ideavimrc

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ cwd=$(pwd)
 cp $cwd/git/gitconfig.template ~/.gitconfig
 cp $cwd/bash/bashrc.template ~/.bashrc
 
-ln -sf $cwd/tmux/tmux.conf ~/.tmux.conf
+ln -sf $cwd/tmux.conf ~/.tmux.conf
 ln -sf $cwd/bash/bash_profile ~/.bash_profile
 
 mkdir -p ~/.vim/backup
-ln -sf $cwd/vim/vimrc ~/.vimrc
+ln -sf $cwd/vimrc ~/.vimrc
 
 mkdir -p ~/.ssh
 ln -sf $cwd/ssh/config ~/.ssh/config
